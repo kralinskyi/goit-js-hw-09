7b9d0a2f1b43448c2840249c1bbcbfa993286012
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-03-17 14:57:44 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+const bodyEl = document.body;
+let timeId = null;
 
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', changeBGcolorOnceInSec);
+stopBtnEl.addEventListener('click', stopChangeBGcolorOnceInSec);
+
+function changeBGcolorOnceInSec() {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+  const { style } = bodyEl;
+
+  timeId = setInterval(() => {
+    style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeBGcolorOnceInSec() {
+  clearInterval(timeId);
+  bodyEl.removeAttribute('style');
+
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
