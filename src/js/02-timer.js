5c3c333c834e3fcb475dd1eb13805e41a86a54ee
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-03-17 16:56:28 +0200

    task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,88 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+// Заміна alert
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const dateInputEl = document.querySelector('#datetime-picker');
+const startBtnEl = document.querySelector('[data-start]');
+const dataDaysEl = document.querySelector('[data-days]');
+const dataHoursEl = document.querySelector('[data-hours]');
+const dataMinutesEl = document.querySelector('[data-minutes]');
+const dataSecondsEl = document.querySelector('[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      Notify.failure('Please choose any date in the future');
+      dateInputEl.style.borderColor = 'red';
+    } else {
+      chosenDate = selectedDates[0];
+
+      startBtnEl.disabled = false;
+      startBtnEl.addEventListener('click', timerOn);
+      dateInputEl.style.borderColor = 'red';
+    }
+  },
+};
+
+startBtnEl.disabled = true;
+
+let chosenDate;
+let timerId;
+
+flatpickr('#datetime-picker', options);
+
+function timerOn() {
+  timerId = setInterval(() => {
+    startBtnEl.disabled = true;
+    dateInputEl.disabled = true;
+    const deltaTime = chosenDate - Date.now();
+
+    if (deltaTime < 1000) {
+      clearInterval(timerId);
+
+      startBtnEl.removeEventListener('click', timerOn);
+
+      dateInputEl.disabled = false;
+      clearInterval(timerId);
+      Notify.info('Repeat?');
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    updateClock({ days, hours, minutes, seconds });
+  }, 1000);
+}
+
+function updateClock({ days, hours, minutes, seconds }) {
+  dataDaysEl.textContent = days;
+  dataHoursEl.textContent = hours;
+  dataMinutesEl.textContent = minutes;
+  dataSecondsEl.textContent = seconds;
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+const addZero = value => String(value).padStart(2, '0');
