32a5a020f0b48464454c27d9a9ff468013a48888
kralinskyi <98835571+kralinskyi@users.noreply.github.com>
2023-04-13 06:26:54 +0000

    Deploying to gh-pages from @ kralinskyi/goit-js-hw-09@cc54e2a3fd27b30a6ada38b7ce2ae4524f226d28 🚀

---
+++ 04-train.4da88a37.js
@@ -0,0 +1,7 @@
+//! import pokemonTmpl from '../templates/pokemon-card.hbs';
+//! const refs = {
+//!   testEl: document.querySelector('.test'),
+//!   form: document.querySelector('.form'),
+//! };
+fetch("https://pixabay.com/api/?key=34842285-9ef26a99ee49cc306160c27d8&q=home").then((function(e){return e.json()})).then(console.log);
+//# sourceMappingURL=04-train.4da88a37.js.map
---
+++ 04-train.f7c82e4b.js
@@ -0,0 +1,7 @@
+//! import pokemonTmpl from '../templates/pokemon-card.hbs';
+//! const refs = {
+//!   testEl: document.querySelector('.test'),
+//!   form: document.querySelector('.form'),
+//! };
+fetch("https://pixabay.com/api/?key=34842285-9ef26a99ee49cc306160c27d8&q=home").then((e=>e.json())).then(console.log);
+//# sourceMappingURL=04-train.f7c82e4b.js.map
